a323ad87423ebeee45cc2c4d9b9e367db0b3ad81
shahd <mazouzchahd12@gmail.com>
2026-02-06 15:32:43 +0100

    feat: Implement full CRUD functionality for categories, tags, and links, including dedicated views, controllers, models, and routes, and integrate them into the dashboard.

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/Register', [RegisterController::class, 'create']);
+Route::get('/register', [RegisterController::class, 'create'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'create'])->name('login');
@@ -17,7 +18,25 @@ Route::post('/login', [LoginController::class, 'store']);
 
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
 
-Route::get('/dashboard', function(){
-    return "welcome" . auth()->user()->name;
-})->middleware('auth');             
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LinkController;
+use App\Http\Controllers\TagController;
 
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::put('/categories/{id}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+    Route::post('/tags', [TagController::class, 'store'])->name('tags.store');
+    Route::get('/tags/{id}/edit', [TagController::class, 'edit'])->name('tags.edit');
+    Route::put('/tags/{id}', [TagController::class, 'update'])->name('tags.update');
+    Route::delete('/tags/{id}', [TagController::class, 'destroy'])->name('tags.destroy');
+
+    Route::post('/links', [LinkController::class, 'store'])->name('links.store');
+    Route::get('/links/{id}/edit', [LinkController::class, 'edit'])->name('links.edit');
+    Route::put('/links/{id}', [LinkController::class, 'update'])->name('links.update');
+    Route::delete('/links/{id}', [LinkController::class, 'destroy'])->name('links.destroy');
+});
