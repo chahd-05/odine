4cbb3973802e23db604cb8ade433b5c6ba98fbeb
shahd <mazouzchahd12@gmail.com>
2026-02-10 12:27:40 +0100

    feat: add the attributes ot role user table

--- database/migrations/2026_02_10_091150_create_role_user_table.php
+++ database/migrations/2026_02_10_091150_create_role_user_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
+            $table->unique(['user_id', 'role_id']);
             $table->timestamps();
         });
     }
