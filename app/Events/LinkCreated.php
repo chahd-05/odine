61f40271e769ba0daa896cf9bdaa5d77d3f05cec
shahd <mazouzchahd12@gmail.com>
2026-02-13 11:52:07 +0100

    feat: add LinkCreated event to signal new link creation.

---
+++ app/Events/LinkCreated.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Link;
+use App\Models\User;
+use Illuminate\Foundation\Events\Dispatchable;
+
+class LinkCreated
+{
+    use Dispatchable;
+
+    public Link $link;
+    public User $user;
+
+    public function __construct(Link $link, User $user)
+    {
+        $this->link = $link;
+        $this->user = $user;
+    }
+}
