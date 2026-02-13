7d4fa9baf2f203570a2fd294503e6076859e8b21
shahd <mazouzchahd12@gmail.com>
2026-02-13 10:20:02 +0100

    feat: add link management, user roles, category/tag management, and activity logging features.

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -3,12 +3,21 @@
 namespace App\Models;
 
 use App\Models\Tag;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Link extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['title', 'url', 'category_id', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function category()
     {
@@ -18,5 +27,15 @@ class Link extends Model
     public function tags()
     {
         return $this->belongsToMany(Tag::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withPivot('access_level')->withTimestamps();
+    }
+
+    public function favoritedBy()
+    {
+        return $this->belongsToMany(User::class, 'favorites')->withTimestamps();
     }
 }
